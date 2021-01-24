9be7708d907ce74f1a54f03ff0bd23a40533d60f
toshinori Ito <tito40358@gmail.com>
2021-01-24 05:38:32 +0000

    重要検索

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -24,6 +24,12 @@ class SearchController < ApplicationController
     @startdate = params[:startdate][:id]
     @finishdate = params[:finishdate][:id]
     @todos = Todo.includes(:accounts).where(user_id:@userid).where('title LIKE ?', "%#{@title}%").where("term >= ?", @startdate).where("term <= ?", @finishdate).order(:term).paginate(page: params[:page], per_page: 20)
+    if params[:finished_only] == "1"
+      @todos = @todos.where(finishday:nil)
+      @finished_state = "未完了対象"
+    else 
+      @finished_state = "全対象"
+    end 
     @kubun = 1
     if @todos.count >= 100
       flash.now[:warning] = "検索結果が100件を超えてます。絞り込みをお願いします"
