77e7b0d351b6e533ea6e7133d882c7d9082d154f
toshinori Ito <tito40358@gmail.com>
2021-03-03 15:05:12 +0000

    weather_repair

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,8 +1,7 @@
-class Weather
+class Weather < Weatheritem
   require "json"
   require "open-uri"
   require 'date'
-  require 'weatheritems'
   API_KEY = "333035987f1db09cade4bb868e15d88a"
   BASE_URL = "http://api.openweathermap.org/data/2.5/forecast"
   
@@ -11,14 +10,20 @@ class Weather
       id = placecode
       response = open(BASE_URL + "?id=#{id}&APPID=#{API_KEY}")
       w_hash = JSON.load(response)
+      now_datetime = DateTime.now
+      day_today =  now_datetime.strftime("%Y-%m-%d")
+      #day_today =  "2021-03-04"
+      #day_hour =  "13"
+      day_hour =  now_datetime.hour
       weather_data = []
-      (4..32).each do |i|
+      (0..31).each do |i|
+        w_day = w_hash["list"][i]["dt_txt"].slice(0..9)
         month = w_hash["list"][i]["dt_txt"].slice(5..6) + "月"
         day = w_hash["list"][i]["dt_txt"].slice(8..9) + "日"
         hour = w_hash["list"][i]["dt_txt"].slice(11..12)
         hour_ja = hour + "時"
         w_name_en = w_hash["list"][i]["weather"][0]["description"]
-        weather_items = Weatheritems.new(w_name_en)
+        weather_items = Weatheritem.new(w_name_en)
         w_name_ja = weather_items.ja_name.to_s
         if w_name_ja == "情報取得失敗"
           w_name_ja = w_name_en
@@ -27,8 +32,9 @@ class Weather
           w_get = true
         end
         icon = weather_items.icon.to_s
-        if hour == "12"
-          weather_data.push([day+hour_ja,w_name_ja,icon,w_get],)
+        #if hour == "12"
+        if (hour == "12"  and (w_day > day_today or (day_hour.to_i <= 11 and w_day == day_today ))) or (day_hour.to_i >= 12 and day_hour.to_i <18 and w_day == day_today and hour == "18")
+          weather_data.push([day+hour_ja,w_name_ja,icon,w_get])
         end
       end
       @return_info = true
