a7b31ad7de8bc1a11d9a5c06dab8c2c996bf5bbb
toshinori Ito <tito40358@gmail.com>
2021-03-03 12:03:57 +0000

    weather_repair

--- app/models/weatheritems.rb
+++ app/models/weatheritems.rb
@@ -3,7 +3,7 @@ class Weatheritems
   weather_items= {
   "clear sky"=>{"ja"=>"快晴", "icon"=>"clearsky.png"},
   "few clouds"=>{"ja"=>"くもり雲少なめ","icon"=>"fewclouds.png"},
-  "broken clouds"=>{"ja"=>"くもりがち","icon"=>"fewclouds.png"},
+  "broken clouds"=>{"ja"=>"くもりがち","icon"=>"brokenclouds.png"},
   "scattered clouds"=>{"ja"=>"くもり雲ふつう","icon"=>"scatteredclouds.png"},
   "overcast clouds"=>{"ja"=>"くもり雲多め","icon"=>"overcastclouds.png"},
   "light intensity shower rain"=>{"ja"=>"小雨のにわか雨","icon"=>"lightintensityshowerrain.png"},
@@ -17,7 +17,7 @@ class Weatheritems
   "thunderstorm"=>{"ja"=>"雷雨","icon"=>"thunderstorm.png"},
   "light snow"=>{"ja"=>"雪","icon"=>"snow.png"},
    "snow"=>{"ja"=>"雪","icon"=>"snow.png"},
-   "mist"=>{"ja"=>"雷雨","icon"=>"thunderstorm.png"},
+   "mist"=>{"ja"=>"雷雨","icon"=>"mist.png"},
    "tornado"=>{"ja"=>"雷雨","icon"=>"thunderstorm.png"},
     }
     begin
