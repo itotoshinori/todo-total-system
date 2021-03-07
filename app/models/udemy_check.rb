0602330d7be7759c0254b8474cb46a64425c77a4
toshinori Ito <tito40358@gmail.com>
2021-03-07 15:12:19 +0000

    udemy_sales_dis_add

--- app/models/udemy_check.rb
+++ app/models/udemy_check.rb
@@ -6,7 +6,8 @@ class Udemy_check
     doc = Nokogiri::HTML.parse(html)
     div_list = doc.search("div")
     sale_coment = div_list.to_s.index("対象コースが")
-    if sale_coment.to_i > 0
+    sale_coment2 = div_list.to_s.index("セール")
+    if sale_coment.to_i > 0 or sale_coment2 > 0
       result = true
     else
       result = false
