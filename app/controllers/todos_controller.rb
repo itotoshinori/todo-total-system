45e1c07da86c9841e66eb74b071d66c904bd3ff7
toshinori Ito <tito40358@gmail.com>
2021-03-08 08:43:19 +0000

    udemy_sales_dis_add

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -30,7 +30,7 @@ class TodosController < ApplicationController
       @kubun = 1
     end
     #Udemyのバーゲンチェック　バーゲンだったら表示＆チャットワーク送信
-    if cookies[:udemy_time_check25].blank? #and request.os == 'Android' and @userid.to_s == "1"
+    if cookies[:udemy_time_check25].blank? and request.os == 'Android' and @userid.to_s == "1"
       begin
         scrap = Scrap_check.new
         url = "https://www.udemy.com/ja"
