5d5312b2b05ac3b5bf778fc6160c4bdce167f4b6
toshinori Ito <tito40358@gmail.com>
2021-03-09 05:17:15 +0000

    udemy_sales_dis_add

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -30,14 +30,15 @@ class TodosController < ApplicationController
       @kubun = 1
     end
     #Udemyのバーゲンチェック　バーゲンだったら表示＆チャットワーク送信
-    if cookies[:udemy_time_check55].blank? #and request.os == 'Android' and @userid.to_s == "1"
+    if cookies[:udemy_time_check56].blank? #and request.os == 'Android' and @userid.to_s == "1"
       begin
         scrap = Scrap_check.new
         url = "https://www.udemy.com/ja"
         title = "Udemyバーゲン購入検討"
-        udemy_check = scrap.check(@userid,url,title,"セール","セール","セール")
-        cookies[:udemy_time_check55] = { :value => true, :expires => 1.days.from_now } 
-        flash[:success]="#{title}が新規登録されました" if udemy_check
+        @udemy_check = scrap.check(@userid,url,title,"セール","セール","セール")
+        #debugger
+        cookies[:udemy_time_check56] = { :value => @udemy_check, :expires => 1.days.from_now } 
+        flash[:success] = "#{title}が新規登録されました" if udemy_check
       rescue => exception
         udemy_check = false
       end
