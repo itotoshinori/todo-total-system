4f889ca6c3dba2e67fa161798c60f3fe7c619cb2
toshinori Ito <tito40358@gmail.com>
2021-03-09 07:45:39 +0000

    udemy_sales_dis_add

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -40,7 +40,7 @@ class TodosController < ApplicationController
         #title = "関西歴史建造物"
         @udemy_check = @scrap.check(@userid,url,title,"勇者ヨシヒコ","対象コース","バーゲン")
         cookies[:udemy_time_check] = { :value => @udemy_check, :expires => 1.days.from_now } 
-        flash[:success] = "#{title}が新規登録されました" if udemy_check
+        flash[:success] = "#{title}が新規登録されました" if @udemy_check
       rescue => exception
         udemy_check = false
       end
