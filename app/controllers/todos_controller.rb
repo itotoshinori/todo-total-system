35a7356f720891c61c87e6f46d1da81e9836d44f
toshinori Ito <tito40358@gmail.com>
2021-03-09 07:37:47 +0000

    udemy_sales_dis_add

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -31,7 +31,7 @@ class TodosController < ApplicationController
     end
     #Udemyのバーゲンチェック　バーゲンだったら表示＆チャットワーク送信
     
-    #if cookies[:udemy_time_check90].blank? and request.os == 'Android' and @userid.to_s == "1"
+    if cookies[:udemy_time_check].blank? and request.os == 'Android' and @userid.to_s == "1"
       begin
         @scrap = Scrap_check.new
         url = "https://www.udemy.com/ja"
@@ -39,12 +39,12 @@ class TodosController < ApplicationController
         title = "Udemyバーゲン購入検討"
         #title = "関西歴史建造物"
         @udemy_check = @scrap.check(@userid,url,title,"勇者ヨシヒコ","対象コース","バーゲン")
-        cookies[:udemy_time_check90] = { :value => @udemy_check, :expires => 1.days.from_now } 
+        cookies[:udemy_time_check] = { :value => @udemy_check, :expires => 1.days.from_now } 
         flash[:success] = "#{title}が新規登録されました" if udemy_check
       rescue => exception
         udemy_check = false
       end
-    #end
+    end
   end
 
   def termindex
