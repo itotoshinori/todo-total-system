66a6df8ba7e1d8d72175601aeb3a14972dcd3b87
toshinori Ito <tito40358@gmail.com>
2021-03-12 11:45:37 +0000

    テレビ番組追加

--- spec/models/tv_schedule_spec.rb
+++ spec/models/tv_schedule_spec.rb
@@ -7,9 +7,13 @@ RSpec.describe Tv_schedule, type: :model do
   end
   #api使用のためテストは不可能
   describe 'スク―ピングテスト' do
-    it 'データを返してくる' do
+    it 'データの結果をテスト' do
+      #一回は取得データを返す
       @tv_schedule = Tv_schedule.new.schedule_add(@user.id)
-      expect(@tv_schedule).not_to be_nil  
+      expect(@tv_schedule).not_to be_nil
+      #二回はデータをnilで返す
+      @tv_schedule = Tv_schedule.new.schedule_add(@user.id)
+      expect(@tv_schedule).to be_nil   
     end
   end
 end
