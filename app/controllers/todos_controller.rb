384fea64304edcdcf796387ca9512a86681c8333
toshinori Ito <tito40358@gmail.com>
2021-03-12 06:33:18 +0000

    テレビ番組追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -37,7 +37,7 @@ class TodosController < ApplicationController
         #url = "http://titonet384.sakura.ne.jp/kokuho/"
         title = "Udemyバーゲン購入検討"
         #title = "関西歴史建造物" テスト用
-        @udemy_check = @scrap.check(@userid,url,title,"セールス","対象コースが","バーゲン")
+        @udemy_check = @scrap.check(@userid,url,title,"セール","対象コースが￥","バーゲン")
         cookies[:udemy_time_check] = { :value => @udemy_check, :expires => 24.hours.from_now } 
         flash[:success] = "#{title}が新規登録されました" if @udemy_check
       rescue => exception
@@ -45,7 +45,7 @@ class TodosController < ApplicationController
       end
       @tv_schedule = Tv_schedule.new.schedule_add(@userid)
     end
-    #@tv_schedule = Tv_schedule.new.schedule_add(@userid)
+    @tv_schedule = Tv_schedule.new.schedule_add(@userid)
   end
 
   def termindex
