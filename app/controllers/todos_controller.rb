4e7cb5186cd976ef00a4d06bbfe7b685cad0e1d0
toshinori Ito <tito40358@gmail.com>
2021-03-17 04:58:56 +0000

    テレビ番組追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -32,15 +32,17 @@ class TodosController < ApplicationController
     #Udemyのバーゲンチェック　バーゲンだったら表示＆チャットワーク送信
     if cookies[:time_check1].blank? and @userid.to_s == "1" #and request.os == 'Android'
       begin
-        @scrap = Scrap_check.new
         url = "https://www.udemy.com/ja"
         #url = "http://titonet384.sakura.ne.jp/kokuho/"
         title = "Udemyバーゲン購入検討"
         #title = "関西歴史建造物" テスト用
         todo_count = Todo.where('title like ?',"%#{title}%").where(term:@date).count
         if todo_count == 0
+          @scrap = Scrap_check.new
           @udemy_check = @scrap.check(@userid,url,title,"セール","対象コースが","バーゲン")
           flash[:success] = "#{title}が新規登録されました" if @udemy_check
+        else
+          flash[:success] = "対象がありません"
         end
         cookies[:time_check1] = { :value => "check", :expires => 1.hours.from_now } 
       rescue => exception
@@ -48,7 +50,7 @@ class TodosController < ApplicationController
       end
       @tv_schedule = Tv_schedule.new.schedule_add(@userid)
     end
-    #@tv_schedule = Tv_schedule.new.schedule_add(@userid)
+    @tv_schedule = Tv_schedule.new.schedule_add(@userid)
   end
 
   def termindex
