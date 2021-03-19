4f93940f04b50ea670c4f262368ebb8d8be5da05
toshinori Ito <tito40358@gmail.com>
2021-03-19 10:27:38 +0000

    テレビ番組追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -48,8 +48,8 @@ class TodosController < ApplicationController
       rescue => exception
         udemy_check = false
       end
-      #@tv_schedule = Tv_schedule.new.schedule_add(@userid)
-      @tv_schedule = Tv_guide.new.schedule_add(@userid)
+      @tv_schedule = Tv_schedule.new.schedule_add(@userid)
+      #@tv_schedule2 = Tv_guide.new.schedule_add(@userid)
     end
     #@tv_schedule = Tv_schedule.new.schedule_add(@userid)
     #@tv_schedule = Tv_guide.new.schedule_add(@userid)
