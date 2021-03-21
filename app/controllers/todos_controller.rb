6f246b8942fee65e147e5de3f8864054a3c00d5d
toshinori Ito <tito40358@gmail.com>
2021-03-21 02:07:54 +0000

    テレビ番組追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -52,7 +52,7 @@ class TodosController < ApplicationController
       @tv_schedule2 = Tv_guide.new.schedule_add(@userid)
     end
     #@tv_schedule = Tv_schedule.new.schedule_add(@userid)
-    #@tv_schedule2 = Tv_guide.new.schedule_add(@userid)
+    @tv_schedule2 = Tv_guide.new.schedule_add(@userid)
   end
 
   def termindex
