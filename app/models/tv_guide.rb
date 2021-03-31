cc505513abdf534db4e2684bbcf85acb326fe13d
toshinori Ito <tito40358@gmail.com>
2021-03-31 08:04:47 +0000

    テレビ番組追加

--- app/models/tv_guide.rb
+++ app/models/tv_guide.rb
@@ -2,12 +2,12 @@
 
 class Tv_guide
   def schedule_add(userid)
-    target_list = ["歴史探偵","巨人 プロ野球","青天を衝（つ）け","新庄剛志","金村義明","朝まで生テレビ","江本孟紀","中田敦彦","サッカー 日本代表","織田信長","重塔","法隆寺","平家","吉永小百合","武田邦彦","豊臣秀吉","池上彰","堀江貴文","西村博之","松村邦洋"] 
+    target_list = ["歴史探偵","巨人 プロ野球 中継","サッカー 日本代表 中継","青天を衝（つ）け","新庄剛志","金村義明","朝まで生テレビ","江本孟紀","中田敦彦","織田信長","重塔","法隆寺","平家","吉永小百合","武田邦彦","豊臣秀吉","池上彰","堀江貴文","西村博之","松村邦洋"] 
     main_titles = []
     tV_program = []
     target_list.each do |target|
-      taeget = URI.encode_www_form(keyword: target)
-      url = "https://www.tvkingdom.jp/schedulesBySearch.action?stationPlatformId=1&condition.#{taeget}"
+      target_url = URI.encode_www_form(keyword: target)
+      url = "https://www.tvkingdom.jp/schedulesBySearch.action?stationPlatformId=1&condition.#{target_url}"
       # urlにアクセスしてhtmlを取得する
       html = URI.open(url).read
       doc = Nokogiri::HTML.parse(html) 
@@ -32,8 +32,8 @@ class Tv_guide
           finishtime = detail.text[20..29].strip
           tv_company = detail.text[50..70].strip
           db_title = title + tv_company + "keyword:" + target
-          todo_count = Todo.where('title like ?',"%#{title}%").where(term:date).count
-          if todo_count == 0 and title.include?("[再]") == false
+          todo_count = Todo.where('body like ?',"%#{url}%").count
+          if todo_count == 0 and title.include?("[再]") == false and title.include?("(仮)") == false
             link = "<a href=https://www.tvkingdom.jp#{url}>テレビ王国</a>"
             @todo = Todo.new(title:db_title, term:date, starttime:starttime, finishtime:finishtime, body:link, user_id:userid)
             tV_program.push([date.to_s + " "  + starttime + "〜" + finishtime , title + tv_company +"word:" + target]) if @todo.save
