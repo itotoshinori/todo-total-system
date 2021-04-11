d250ffaeab6a1178cfa298c95fa3c401e6f42529
toshinori Ito <tito40358@gmail.com>
2021-04-11 08:41:35 +0000

    現在の時刻追加

--- app/javascript/components/Clock.js
+++ app/javascript/components/Clock.js
@@ -1,10 +1,18 @@
 import React from "react"
 import PropTypes from "prop-types"
+var today = new Date();
+var todayDay = today.getMonth() + 1
+var tomonth = today.getMonth() + 1
+var todate = today.getDate();
+var dayOfWeekStr = ["日", "月", "火", "水", "木", "金", "土"][today.getDay()];
 
 class Clock extends React.Component{
  constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = {
+      date: new Date(),
+      thisday:""
+    };
   }
 
   componentDidMount() {
@@ -20,14 +28,17 @@ class Clock extends React.Component{
 
   tick() {
     this.setState({
-      date: new Date()
+      date: new Date(),
+      tomonth:  todayDay,
+      today:todate,
+      weekday:dayOfWeekStr
     });
   }
 
   render() {
     return (
       <div>
-        <div>現在の時刻は: {this.state.date.toLocaleTimeString()}.</div>
+        <div>今の時刻: {this.state.tomonth}月{this.state.today}日({this.state.weekday}) {this.state.date.toLocaleTimeString()}</div>
       </div>
     );
   }
