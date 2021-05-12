e6880bff85895245018fe087bb402c915f1a70c4
toshinori Ito <tito40358@gmail.com>
2021-05-12 06:13:25 +0000

    パスワードシステム簡易化

--- spec/models/image_auth_spec.rb
+++ spec/models/image_auth_spec.rb
@@ -7,23 +7,14 @@ RSpec.describe ImageAuth, type: :model do
   end
   describe 'ランダム画像表示テスト' do
     it '期待通りに結果がでる1' do
-      expect(@imageauth.auth_dis).to eq "l1.jpg"
-      expect(@imageauth.auth_dis_word).to eq "4l3hw@65!"
+      expect(@imageauth.auth_dis).to eq "sec0.gif"
+      expect(@imageauth.auth_dis_word).to eq "4554"
       @imageauth = ImageAuth.new(@num += 1)
-      expect(@imageauth.auth_dis).to eq "l2.jpg"
-      expect(@imageauth.auth_dis_word).to eq "3kR23*$2"
+      expect(@imageauth.auth_dis).to eq "sec1.gif"
+      expect(@imageauth.auth_dis_word).to eq "2209"
       @imageauth = ImageAuth.new(@num += 1)
-      expect(@imageauth.auth_dis).to eq "l3.gif"
-      expect(@imageauth.auth_dis_word).to eq "5*+75RtY&"
-      @imageauth = ImageAuth.new(@num += 1)
-      expect(@imageauth.auth_dis).to eq "l4.gif"
-      expect(@imageauth.auth_dis_word).to eq "+LasW#3$2"
-      @imageauth = ImageAuth.new(@num += 1)
-      expect(@imageauth.auth_dis).to eq "l5.gif"
-      expect(@imageauth.auth_dis_word).to eq "+95lJKJ?"
-      @imageauth = ImageAuth.new(@num += 1)
-      expect(@imageauth.auth_dis).to eq "l6.gif"
-      expect(@imageauth.auth_dis_word).to eq "&YnNa$MkV"
+      expect(@imageauth.auth_dis).to eq "sec2.gif"
+      expect(@imageauth.auth_dis_word).to eq "3981"
     end
   end
 end
