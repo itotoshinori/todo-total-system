d5e47929ba53227f2fb04f42aad6dff512a3da5c
toshinori Ito <tito40358@gmail.com>
2021-06-04 02:19:53 +0000

    ｲ-ナム修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -16,7 +16,7 @@ class Category < ApplicationRecord
   契約書:11,
   ブックマーク:12,
   転職活動:13,
-  健康管理:14}
+  プロジェクト:14}
 
   def category_array(todo)
     if todo.category_id.present? or todo.category_id2.present? or todo.category_id3.present?
