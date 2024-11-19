913cd6d396285c8737e3a483f5cb48b05aeb3e2b
T-I <tito40358@gmail.com>
2024-11-19 14:39:36 +0900

    first-commit

---
+++ resources/ts/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root'); // IDはBladeテンプレートと一致させます
+if (!container) throw new Error('Failed to find the root element');
+
+const root = createRoot(container);
+root.render(<App />);
