ea87e97aa4f26696f25d13bb526d560d3dae8767
T-I <tito40358@gmail.com>
2024-11-30 17:15:37 +0900

    タイトル更新

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel On React</title>
+    <title>タスク管理システム</title>
     @viteReactRefresh
     @vite('resources/ts/index.tsx')
     @vite('resources/css/app.css')
